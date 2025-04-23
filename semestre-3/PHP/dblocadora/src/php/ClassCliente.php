e76a1f30dd225f79cd7a00f3fbc7429c479791cd
otavio <aapo5719@gmail.com>
2025-04-23 15:43:04 -0300

    conexao com banco

--- semestre-3/PHP/dblocadora/src/php/ClassCliente.php
+++ semestre-3/PHP/dblocadora/src/php/ClassCliente.php
@@ -2,9 +2,9 @@
 
 class Cliente{
    
-    private$nome;
-    private  $cpf;
-    private$endereco;
+    private $nome;
+    private $cpf;
+    private $endereco;
     private $conexao;
 
     public function __construct($nome, $cpf, $endereco, $conexao){
@@ -20,6 +20,7 @@ class Cliente{
         $stmt = $this->conexao->prepare($sql);
         $stmt->bind_param('sss', $this->nome, $this->cpf, $this->endereco);
         if($stmt->execute()){
+
             echo "cliente inserido";
 
 
@@ -30,6 +31,40 @@ class Cliente{
         }
 
     $stmt->close();
+    }
+    Public function listarCliente(){
+
+
+        $sql = "SELECT cliente_nome, cliente_cpf , cliente_endereco FROM tbclientes";
+
+        $resultado = $this->conexao->query($sql);
+        if ($resultado->num_rows>0) {
+            echo "<h3>Listagem de clientes</h3><table>";
+            echo "<th>nome</th><th>CPF</th><th>endereço</th>";
+            foreach($resultado as $row){
+                $nm = $row['nome'];
+                $cpf = $row['email'];
+                $end = $row['msg'];
+                echo "<tr>
+                
+                <td>$nm</td> 
+                <td>$cpf/td>
+                <td>$end</td>
+                
+                
+                </tr>";
+                echo "</table>";
+            }
+
+                
+
+
+
+        }
+
+
+
+        
     }
 }
 
--- semestre-3/PHP/dblocadora/src/php/global.php
+++ semestre-3/PHP/dblocadora/src/php/global.php
@@ -1,13 +1,17 @@
 <?php
-
+include_once 'ClassCliente.php';
+include_once 'dbconnect.php';
 if(isset($_POST['cadastrar-cliente'])){
     $nome = $_POST['nome-cliente'];
     $cpf = $_POST['cliente-cpf'];
     $endereco = $_POST['cliente-endereco'];
+    $cliente = new Cliente($nome, $cpf, $endereco, $conexao);
+    $cliente->insereCliente();
 
-    echo $nome;
-    echo $cpf;
-    echo $endereco;
+    
 }
-
+if(isset($_POST['Listar']))
+$nome="";$endereco="";$cpf='';
+$cliente = new Cliente($nome, $cpf, $endereco, $conexao);
+$cliente-> listarCliente();
 ?>
