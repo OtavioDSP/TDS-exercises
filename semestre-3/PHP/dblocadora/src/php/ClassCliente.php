33a23551b7a9662057a2cf92e2e84116a829be42
otavio <aapo5719@gmail.com>
2025-04-22 21:15:49 -0300

    iniciando trabalho

---
+++ semestre-3/PHP/dblocadora/src/php/ClassCliente.php
@@ -0,0 +1,40 @@
+<?php
+
+class Cliente{
+   
+    private$nome;
+    private  $cpf;
+    private$endereco;
+    private $conexao;
+
+    public function __construct($nome, $cpf, $endereco, $conexao){
+
+        $this->nome = $nome;
+        $this->cpf = $cpf;
+        $this->endereco= $endereco;
+        $this->conexao = $conexao;
+    }
+    public function insereCliente(){
+        $sql = "INSERT INTO tb_cliente(nome, cpf, endereco) VALUES (?,?,?)";
+        
+        $stmt = $this->conexao->prepare($sql);
+        $stmt->bind_param('sss', $this->nome_cli, $this->cpf_cli, $this->endereco_cli);
+        if($stmt->execute()){
+            echo "cliente inserido";
+
+
+        }else{
+
+            echo "erro ao inserir". $stmt->error;
+
+        }
+
+    $stmt->close();
+    }
+}
+
+
+
+
+
+?>
---
+++ semestre-3/PHP/dblocadora/src/php/global.php
@@ -0,0 +1,8 @@
+<?php
+if(isset($_POST['cadastrar-cliente'])){
+    $nome = $_POST['nome-cliente'];
+    $cpf = $_POST['cliente-cpf'];
+    $endereco = $_POST['cliente-endereco'];
+}
+
+?>
