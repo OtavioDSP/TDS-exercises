534af1a0513acd774c57f198d56f8297435d5a65
otavio <aapo5719@gmail.com>
2025-05-04 22:48:07 -0300

    iniciando integração com frontend

--- semestre-3/PHP/dblocadora/cadastrar.php
+++ semestre-3/PHP/dblocadora/cadastrar.php
@@ -1,6 +1,5 @@
 <?php
 include_once 'src/php/dbconnect.php';
-
 ?>
 
 <!DOCTYPE html>
@@ -8,83 +7,67 @@ include_once 'src/php/dbconnect.php';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="src/css/cadastrarstyle.css">
+    <title>Cadastro</title>
 </head>
 <body>
 
+<header>
+    <div class="header-left">
+        <img src="src/img/logo.png" alt="Logo" class="logo">
+    </div>
 
-    <form action="src/php/global.php" method="post">
-        <input type="text" name="cliente-cpf" placeholder="Seu nome">
-        <input type="text" name="nome-cliente" placeholder="Seu CPF">
-        <input type="text" name="cliente-endereco" placeholder="Seu endereço">
-        <input type="submit" value="Cadastrar" name="cadastrar-cliente">
+    <div class="header-center">
+        <nav class="nav-links">
+            <a href="index.html">Início</a>
+            <a href="verificar-veiculos.html">Verificar Registros</a>
+        </nav>
+    </div>
 
-        <input type="submit" value="Listar" name="listar-cliente">
-    </form>
-
-    <form action="src/php/global.php" method="post">
-
-        <input type="text" name="carro_desc" placeholder="Carro">
-        <select name="carro_marca" id="">
-            <option value=""> Informe a Marca</option>
-            <?php
-
-            
-            
-            
-            $sql = "SELECT marca_codigo, marca_descricao  FROM tbmarca";
-            
-            
-            $resultado = $conexao->query($sql);
-            if ($resultado->num_rows>0) {
-               foreach($resultado as $row){
-                   
-                    $codigo = $row['marca_codigo'];
-                    $desc = $row['marca_descricao'];
-
-                    echo "<option value='$codigo'>$desc</option>";
-                }
-    
-            }
-?>
-
-        </select>
-        <input type="text" name="carro_placa" placeholder="placa do veiculo.">
-        <input type="submit" value="Cadastrar" name="cadastrar-carro">
-
-        <input type="submit" value="Listar" name="listar-carro">
+    <div class="header-right">
+        <h1>Moon Runner<br>Locadora de Veículos</h1>
+    </div>
+</header>
 
 
+<div class="form-container">
+    <div class="form">
+        <form action="src/php/global.php" method="post">
+            <input type="text" name="cliente-cpf" placeholder="Seu nome">
+            <input type="text" name="nome-cliente" placeholder="Seu CPF">
+            <input type="text" name="cliente-endereco" placeholder="Seu endereço">
+            <div class="form-buttons">
+                <input type="submit" value="Cadastrar" name="cadastrar-cliente">
+                <input type="submit" value="Listar" name="listar-cliente">
+            </div>
+        </form>
+    </div>
 
-    </form>
-
-
-    <form action="src/php/global.php" method="post">
-    <h3>Cadastrar Locação</h3>
-        <select name="cliente_loc" id="">
-        <option value=""> Informe o Cliente</option>
-            <?php
-            
-            $sql = "SELECT cliente_nome, cliente_cpf FROM tbcliente";
-                
-                
+    <div class="form">
+        <form action="src/php/global.php" method="post">
+            <input type="text" name="carro_desc" placeholder="Carro">
+            <select name="carro_marca">
+                <option value="">Informe a Marca</option>
+                <?php
+                $sql = "SELECT marca_codigo, marca_descricao FROM tbmarca";
                 $resultado = $conexao->query($sql);
-                if ($resultado->num_rows>0) {
-                foreach($resultado as $row){
-                    
-                        $nome = $row['cliente_nome'];
-                        $cpf = $row['cliente_cpf'];
-
-                        echo "<option value='$cpf'>$nome</option>";
+                if ($resultado->num_rows > 0) {
+                    foreach($resultado as $row){
+                        $codigo = $row['marca_codigo'];
+                        $desc = $row['marca_descricao'];
+                        echo "<option value='$codigo'>$desc</option>";
                     }
-        
                 }
-            
-            
-            
-            
-            ?>
+                ?>
             </select>
+            <input type="text" name="carro_placa" placeholder="Placa do veículo.">
+            <div class="form-buttons">
+                <input type="submit" value="Cadastrar" name="cadastrar-carro">
+                <input type="submit" value="Listar" name="listar-carro">
+            </div>
+        </form>
+    </div>
+</div>
             
             
             <select name="veiculo_loc" id="">
