3ad925d80de68604aaa900a0b8fe6272772167c4
otavio <aapo5719@gmail.com>
2025-06-17 12:05:24 -0300

    iniciando atv 01

--- semestre-3/js/lista-1/scripts/ex1.js
+++ semestre-3/js/lista-1/scripts/ex1.js
@@ -1,1 +1,18 @@
-let 
+const qtdQuartos = () => {
+    let num = Math.floor(Math.random() * 11);
+    document.getElementById('placeholder').innerText = num;
+    return num;
+};
+const led = ()=>{
+    if(qtdQuartos == 0){
+        document.getElementById('led').background.color = "blue";
+    }else{
+        document.getElementById('led').style.color = "green";
+    }
+}
+const atualizar = () => {
+    const valor = qtdQuartos(); 
+    led(valor);
+};
+
+window.onload = atualizar();
