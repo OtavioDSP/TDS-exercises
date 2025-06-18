a94a9bac0a0f272c0d4392635ab1dacecceb0d01
otavio <aapo5719@gmail.com>
2025-06-18 10:18:18 -0300

    att

--- semestre-3/js/lista-1/scripts/ex02.js
+++ semestre-3/js/lista-1/scripts/ex02.js
@@ -1,6 +1,8 @@
-elements = document.querySelectorAll('.lampada');
+element = document.querySelectorAll('.lampada');
+element2 = document.querySelectorAll('.interruptor');
 bgLampada = window.getComputedStyle(lampada).backgroundImage;
-const mudaEstado = (url, bg) => {
+bgLampada = window.getComputedStyle(lampada).backgroundImage;
+const mudaEstado = (elem, elem2, bg, bg2) => {
     
 
 
