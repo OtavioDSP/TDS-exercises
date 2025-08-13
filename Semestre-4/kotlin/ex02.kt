30c8a1cc324c8c90478c62ea88ebe5c49624b1b0
otavio <aapo5719@gmail.com>
2025-08-13 16:53:13 -0300

    exercicios de kotlin

---
+++ Semestre-4/kotlin/ex02.kt
@@ -0,0 +1,32 @@
+import java.util.*
+ 
+fun main(){
+	
+	var soma: Float = 0
+	var sub: Float = 0
+	var multp: Float = 0 
+	var div: Float = 0
+	var num1: Float = 0
+	var num2: Float = 0
+	
+	printLn("Digite o num 1")
+	num1 - readline()!!.toInt()
+	printLn("digite o num 2")
+	num1 - readline()!!.toInt()
+	
+	soma = num1+num2
+	multip = num1*num2
+	sub = num1-num2
+	div = num1/num2
+	
+	printLn("Soma $soma")
+	printLn("sub $sub")
+	printLn("multp $mulp")
+	printLn("div $div")
+	
+	
+	
+	
+	
+	
+}
