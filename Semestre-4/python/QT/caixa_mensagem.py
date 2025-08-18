b378f22a343eda83108315481bda724d37204e66
otavio <aapo5719@gmail.com>
2025-08-18 17:24:16 -0300

    atv 03 qt

--- Semestre-4/python/QT/caixa_mensagem.py
+++ Semestre-4/python/QT/caixa_mensagem.py
@@ -19,21 +19,52 @@ def fechar_tela():
         janela.close()
 
 def listar_dados():
-    dado_lido = janela.lineEdit.text()
-    janela.lista.addItem(dado_lido)
-
+#Nome
+    dado_lido = janela.LeNome.text()
+#Confere Nome se n estiver vazio
+    if dado_lido=="":
+        QMessageBox.about(janela,"Nome:", "Nenhum nome foi digitado")
+    else:
+#Idade
+        dado_lido2 = janela.LeIdade.text()
+#Confere Idade se n estiver vazio
+    if dado_lido2=="":
+        QMessageBox.about(janela,"Idade:", "Idade Vazia")
+    else:
+#Sexo
+        if janela.rb1.isChecked():
+            dado_lido3= 'M'
+        elif janela.rb2.isChecked():
+            dado_lido3= 'F'
+        else:
+            dado_lido3= 'ND'
+#Escolaridade
+    dado_lido4=janela.cbnivel.currentText()
+#Juntar tudo
+    dados = "Nome: "+dado_lido+"\nIdade: "+dado_lido2+"\nSexo: "+dado_lido3+"\nEscolaridade: "+dado_lido4
+#Limpar
+    janela.lenome.setText("")
+    janela.leidade.setText("")
+#Adicionar a Lista msm
+    janela.lista.addItem(dados)
+#Contador
+    contar_itens()
+  
 def deletar():
-    
-    janela.lista.clear()
-
+    result = QMessageBox.question(janela, "Excluir Itens", "Deseja mesmo excluir todos os itens da Lista?", QMessageBox.Yes, QMessageBox.No)
+    if result == QMessageBox.Yes:
+        janela.lista.clear()
+        contar_itens()
 def contar_itens():
-
     valor = janela.lista.count()
-    janela.lbconta.setText("Qtde de itens: " + str(valor))
-
+    janela.lbconta.setText("Nº de Itens: " + str(valor))
 def ordenar():
-    
     janela.lista.sortItems()
+
+
+
+
+    
         
 app=QtWidgets.QApplication([])
 janela=uic.loadUi("caixa_mensagem.ui")
